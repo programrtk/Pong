9bdac785a2a91dd18e93a5506a3cf067d625753f
guptas5279 <guptas5279@CA-SU-F106-11.sduhsd.lan>
2019-04-15 09:29:34 -0700

    first commit

---
+++ src/PongFiles/Locatable.java
@@ -0,0 +1,17 @@
+package PongFiles;
+
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+public interface Locatable {
+
+    public void setPos(int x, int y);
+
+    public void setxPos(int x);
+
+    public void setyPos(int y);
+
+    public int getxPos();
+
+    public int getyPos();
+}
